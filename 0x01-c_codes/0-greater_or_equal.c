7228385b077ad2f106b7e1a0b2ffb96a4af1a0df
G4GIFTEDHANDS <113684052+G4GIFTEDHANDS@users.noreply.github.com>
2022-12-21 16:38:10 +0100

    Update 0-greater_or_equal.c

--- 0x01-c_codes/0-greater_or_equal.c
+++ 0x01-c_codes/0-greater_or_equal.c
@@ -1,33 +1,47 @@
-# prompt the user for input
-print("Hello, how are you today?")
-# get the user's response
-response = input()
+char response[100];
+char begin[100];
+int num1;
+int num2;
 
-# prompt the user to begin
-print("Shall we begin?")
-# get the user's response
-begin = input()
+// prompt the user for input
+printf("Hello, how are you today?\n");
+// get the user's response
+scanf("%s", response);
 
-# prompt the user for the first number
-print("What's the first number?")
-# get the first number from the user
-num1 = int(input())
+// prompt the user to begin
+printf("Shall we begin?\n");
+// get the user's response
+scanf("%s", begin);
 
-# prompt the user for the second number
-print("What's the second number?")
-# get the second number from the user
-num2 = int(input())
+// prompt the user for the first number
+printf("What's the first number?\n");
+// get the first number from the user
+scanf("%d", &num1);
 
-# compare the two numbers
-if num1 == num2:
-  # print that the numbers are equal
-  print("The numbers are equal.")
-elif num1 > num2:
-  # print that the first number is larger
-  print(f"{num1} is larger than {num2}.")
-else:
-  # print that the second number is larger
-  print(f"{num2} is larger than {num1}.")
+// prompt the user for the second number
+printf("What's the second number?\n");
+// get the second number from the user
+scanf("%d", &num2);
 
-# print the final message
-print("Wow!!! We make an awesome team!!! High five dude!!!")
+// compare the two numbers
+if (num1 == num2)
+{
+// print that the numbers are equal
+printf("The numbers are equal.\n");
+}
+else if (num1 > num2)
+{
+// print that the first number is larger
+printf("%d is larger than %d.\n", num1, num2);
+}
+else
+{
+// print that the second number is larger
+printf("%d is larger than %d.\n", num2, num1);
+}
+
+// print the final message
+printf("Wow!!! We make an awesome team!!! High five dude!!!\n");
+
+return 0;
+}
